eb1c69eecca9939b252758f512ebdf983de885ab
Nicholas Hydock <nhydock@gmail.com>
2014-07-18 18:05:13 -0400

    Forgot to commit progress element for hud

---
+++ src/scenes/dungeon/Progress.java
@@ -0,0 +1,14 @@
+package scenes.dungeon;
+
+public class Progress {
+
+	public int monstersKilled;
+	public int monstersTotal;
+	
+	public int lootFound;
+	public int lootTotal;
+	
+	public int depth;
+	public int floors;
+	
+}
